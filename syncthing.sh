d8487c5d8ca578685e1dbf6a923df98a8371537a
Connor Glosser <glosser1@gmail.com>
2018-11-22 12:03:57 -0500

    Add syncthing install

---
+++ syncthing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Add the release PGP keys:
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+
+# Add the "stable" channel to your APT sources:
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install syncthing
+
+# Add syncthing to systemd startup: 
+wget -P /tmp/ https://raw.githubusercontent.com/syncthing/syncthing/master/etc/linux-systemd/system/syncthing%40.service
+sudo mv /tmp/syncthing@.service /etc/systemd/system/syncthing@.service
+sudo systemctl enable syncthing@connor.service
+sudo systemctl start syncthing@connor.service
